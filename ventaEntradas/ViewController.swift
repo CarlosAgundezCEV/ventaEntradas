d6a946ff9893863c4ea324ad810152858ae1fe62
CarlosAgundezCEV <carlos_agundez_apps1ma1718@cev.com>
2018-11-23 11:50:58 +0100

    Initial Commit

---
+++ ventaEntradas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ventaEntradas
+//
+//  Created by wizO on 23/11/2018.
+//  Copyright © 2018 Carlos Agundez Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
